54e39bfc34b486b2ef7e04f1c448bfe856f2f8ef
Alber <betomiku@gmail.com>
2025-02-25 20:05:37 +0100

    Detach ball when leaving mouse.

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,7 +5,10 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D currentBallRigidBody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float detachDuration = 0.2F;
     private Camera mainCamera;
+    private bool isDragging;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,25 +18,68 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!currentBallRigidBody)
+        if (currentBallRigidBody == null)
         {
             return;
         }
 
         if (Touchscreen.current.primaryTouch.press.IsPressed())
         {
-            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-
-            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-
-            currentBallRigidBody.position = worldPosition;
-            currentBallRigidBody.bodyType = RigidbodyType2D.Kinematic;
-
-            Debug.Log(worldPosition);
+            SetBallRigidBodyAsKinematic();
+            SetIsDragging();
+            SetBallLocation();
         }
         else
         {
-            currentBallRigidBody.bodyType = RigidbodyType2D.Dynamic;
+            SetBallRigidBodyAsDynamic();
+            if (isDragging)
+            {
+                ResetIsDragging();
+                LaunchBall();
+            }
         }
+    }
+
+    private void SetBallRigidBodyAsDynamic()
+    {
+        currentBallRigidBody.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    private void SetBallLocation()
+    {
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidBody.position = worldPosition;
+
+        Debug.Log(worldPosition);
+    }
+
+    private void SetBallRigidBodyAsKinematic()
+    {
+        currentBallRigidBody.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    private void SetIsDragging()
+    {
+        isDragging = true;
+    }
+
+    void LaunchBall()
+    {
+        Invoke("DetachBall", detachDuration);
+        currentBallRigidBody = null;
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
+    }
+
+    private void ResetIsDragging()
+    {
+        isDragging = false;
     }
 }
