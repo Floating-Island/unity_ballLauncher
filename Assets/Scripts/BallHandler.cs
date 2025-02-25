1629861d86191ff2cad0674923e79cbc7efcefd6
Alber <betomiku@gmail.com>
2025-02-25 19:22:31 +0100

    Log world position of screen touch

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -4,10 +4,11 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    private Camera mainCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -16,7 +17,10 @@ public class BallHandler : MonoBehaviour
         if (Touchscreen.current.primaryTouch.press.IsPressed())
         {
             Vector2 TouchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-            Debug.Log(TouchPosition);
+
+            Vector3 WorldPosition = mainCamera.ScreenToWorldPoint(TouchPosition);
+
+            Debug.Log(WorldPosition);
         }
     }
 }
