4f6857f22c477c2410be0ffb4f924258adc5bf2f
Alber <betomiku@gmail.com>
2025-02-25 20:32:09 +0100

    Add ball respawning.

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -4,15 +4,20 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D currentBallRigidBody;
-    [SerializeField] private SpringJoint2D currentBallSpringJoint;
-    [SerializeField] private float detachDuration = 0.2F;
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Rigidbody2D pivot;
+    [SerializeField] private float detachDelay = 0.1F;
+    [SerializeField] private float respawnDelay = 0.5F;
+    
+    private Rigidbody2D currentBallRigidBody;
+    private SpringJoint2D currentBallSpringJoint;
     private Camera mainCamera;
     private bool isDragging;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         mainCamera = Camera.main;
+        SpawnNewBall();
     }
 
     // Update is called once per frame
@@ -66,9 +71,9 @@ public class BallHandler : MonoBehaviour
         isDragging = true;
     }
 
-    void LaunchBall()
+    private void LaunchBall()
     {
-        Invoke(nameof(DetachBall), detachDuration);
+        Invoke(nameof(DetachBall), detachDelay);
         currentBallRigidBody = null;
     }
 
@@ -76,10 +81,22 @@ public class BallHandler : MonoBehaviour
     {
         currentBallSpringJoint.enabled = false;
         currentBallSpringJoint = null;
+
+        Invoke(nameof(SpawnNewBall), respawnDelay);
     }
 
     private void ResetIsDragging()
     {
         isDragging = false;
+    }
+
+    private void SpawnNewBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+
+        currentBallRigidBody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+
+        currentBallSpringJoint.connectedBody = pivot;
     }
 }
